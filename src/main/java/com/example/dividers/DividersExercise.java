b8acf7de2c7e8d726711e75975100ee48352cb5d
unluckypizzu <emiliopizzuto99@gmail.com>
2023-07-04 15:42:50 +0200

    PrimeNumbers
    Calcola i numeri primi compresi tra due numeri interi inseriti dall'utente.

--- src/main/java/com/example/dividers/DividersExercise.java
+++ src/main/java/com/example/dividers/DividersExercise.java
@@ -1,13 +1,45 @@
-package com.example.demo;
+package com.example.dividers;
 
-import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
+import java.util.Scanner;
 
-@SpringBootApplication
-public class DividersApplication {
+public class DividersExercise {
+	
+	// Scrivere un programma che richiesto un numero intero visualizzi tutti i suoi divisori, contandoli.  
 
 	public static void main(String[] args) {
-		SpringApplication.run(DividersApplication.class, args);
+		
+        int dividers = 0;
+        Boolean inputInt = false;
+        int input = 0;
+        
+		Scanner scanner = new Scanner(System.in);
+        
+        
+        while (!inputInt) {
+            System.out.print("Inserisci un numero intero: ");
+
+            if (scanner.hasNextInt()) {
+                input = scanner.nextInt();
+                inputInt = true;
+            } else {
+                System.out.println("Input non valido.");
+                scanner.next();
+            }
+        }
+        
+        scanner.close();
+
+        System.out.println("I divisori di " + input + " sono: ");
+        for (int i = 1; i <= input; i++) {
+            if (input % i == 0) {
+                System.out.print(i + " ");
+                dividers++;
+            }
+        }
+
+        System.out.print("\n");
+        
+        System.out.println("Il numero " + input + " ha " + dividers + " divisori.");
 	}
 
 }
---
+++ src/main/java/com/example/primenumbers/PrimeNumbers.java
@@ -0,0 +1,72 @@
+package com.example.primenumbers;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+import java.util.function.IntPredicate;
+import java.util.function.Predicate;
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
+
+public class PrimeNumbers {
+	
+	// Calcola i numeri primi compresi tra due numeri interi inseriti dall'utente. 
+
+	public static void main(String[] args) {
+		Boolean isInputInt = false;
+		int firstNumber = 0;
+		int secondNumber = 0;
+		
+		Predicate<Integer> primeNumberPredicate = primeNumbers -> {
+			if (primeNumbers <= 1) {
+	            return false;
+	        }
+			
+			for (int i = 2; i < primeNumbers; i++) {
+	            if (primeNumbers % i == 0) {
+	                return false;
+	            }
+	        }
+			
+			return true;
+		};
+		
+		IntPredicate primeNumberIntPredicate = number -> primeNumberPredicate.test(number);
+		
+		Scanner scanner = new Scanner(System.in);
+        
+        
+        while (!isInputInt) {
+            System.out.print("Inserisci il primo numero intero: ");
+
+            if (scanner.hasNextInt()) {
+            	firstNumber = scanner.nextInt();
+                isInputInt = true;
+            } else {
+                System.out.println("Input non valido.");
+                scanner.next();
+            }
+        }
+        
+        isInputInt = false;
+        
+        
+        while (!isInputInt) {
+            System.out.print("Inserisci il secondo numero intero: ");
+
+            if (scanner.hasNextInt()) {
+            	secondNumber = scanner.nextInt();
+                isInputInt = true;
+            } else {
+                System.out.println("Input non valido.");
+                scanner.next();
+            }
+        }
+		
+        
+        System.out.println("I numeri primi tra " + firstNumber + " e " + secondNumber + " sono:");
+        IntStream.rangeClosed(firstNumber, secondNumber).filter(primeNumberIntPredicate).forEach(System.out::println);
+		
+	}
+
+}
